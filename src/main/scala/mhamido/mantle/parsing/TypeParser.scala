d0752b57def6e0a5e554d7f230c1534f91eccb8d
mhamido <mahmoudmhamido@gmail.com>
2023-05-02 00:56:08 +0300

    Fixed span output by type parser

--- src/main/scala/mhamido/mantle/parsing/TypeParser.scala
+++ src/main/scala/mhamido/mantle/parsing/TypeParser.scala
@@ -9,7 +9,8 @@ trait TypeParser extends Parser {
     peek {
       case Token.ThinArrow =>
         val arrow = advance()
-        Type.Fn(base, tpe())(using ???)
+        val ret = tpe()
+        Type.Fn(base, ret)(using base.info <> ret.info)
       case _ => base
     }
   }
@@ -20,17 +21,21 @@ trait TypeParser extends Parser {
     while !isAtEnd && matches(primStarts*) do argsBuilder += peek(prim)
     argsBuilder.result() match {
       case Nil => fn
-      case xs  => Type.App(fn, xs)(using ???)
+      case xs  => Type.App(fn, xs)(using fn.info <> pos)
     }
   }
 
-  val primStarts: Seq[Token.Kind] = Seq(Token.OpenParen, Token.LowerName, Token.UpperName)
+  val primStarts: Seq[Token.Kind] =
+    Seq(Token.OpenParen, Token.LowerName, Token.UpperName)
 
   val prim: PartialFunction[Token.Kind, Type] = {
     case Token.OpenParen =>
       val openParen = advance()
       peek {
-        case Token.CloseParen => Type.Unit()(using ???)
+        case Token.CloseParen =>
+          val closeParen = advance()
+          Type.Unit()(using openParen.pos <> closeParen.pos)
+
         case _ =>
           val elems = List.newBuilder[Type]
           elems += tpe()
@@ -41,16 +46,16 @@ trait TypeParser extends Parser {
 
           elems.result() match
             case List(x) => x
-            case xs      => Type.Tuple(xs)(using ???)
+            case xs      => Type.Tuple(xs)(using xs.head.info <> xs.last.info)
       }
 
     case Token.LowerName =>
       val name = advance()
-      Type.Var(name.literal)(using ???)
+      Type.Var(name.literal)(using name.pos <> pos)
 
     case Token.UpperName =>
       val name = advance()
-      Type.Named(name.literal)(using ???)
+      Type.Named(name.literal)(using name.pos <> pos)
   }
 }
 
