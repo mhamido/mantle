66be2d7cbcead316f2d9217ffbc84e215557dd57
mhamido <mahmoudmhamido@gmail.com>
2023-09-15 01:53:57 +0300

    [WIP] impl of the type checker

--- src/main/scala/mhamido/mantle/TypeChecker.scala
+++ src/main/scala/mhamido/mantle/TypeChecker.scala
@@ -1,32 +1,75 @@
 package mhamido.mantle
 
+import scala.collection.immutable.HashMap
+import mhamido.mantle.util.Phase
+import mhamido.mantle.util.Context
+
 object typed extends AbstractModule {
-  type Name = syntax.Name
+  type Name = Symbol
   type Info = Type
 }
 
-class TypeChecker() {
-  import syntax.{Expr, Decl, Type}
+class TypeChecker:
+  import TypeChecker.*
+  import syntax.{Decl, Expr, Type, Param}
 
-  type Env = Map[syntax.Name, typed.Type]
-  case class Typecheck[A]()
+  given Conversion[syntax.Type, typed.Type] = ???
+  given Conversion[syntax.Name, typed.Name] = Symbol(_)
 
-  def infer(expr: syntax.Expr)(using env: Env): Typecheck[typed.Expr] = expr match
-    case syntax.Expr.Not(value) => {
-      val tpedValue = check(value, syntax.Type.Boolean)
-      pure { }
-    }
+  def infer(expr: Expr)(using Environment): typed.Expr =
+    given typed.Info = ???
+    expr match
+      case Expr.Unit()                => typed.Expr.Unit()
+      case Expr.True()                => typed.Expr.True()
+      case Expr.False()               => typed.Expr.False()
+      case Expr.Var(name)             => ???
+      case Expr.Int(value)            => typed.Expr.Int(value)
+      case Expr.Chr(value)            => typed.Expr.Chr(value)
+      case Expr.String(value)         => typed.Expr.String(value)
+      case Expr.Not(subexpr)          => ???
+      case Expr.Negate(subexpr)       => ???
+      case Expr.Bin(op, lhs, rhs)     => ???
+      case Expr.Ascribe(subexpr, tpe) => check(subexpr, tpe)
+      case Expr.Apply(fn, arg)        => ???
+      case Expr.If(cond, thenp, elsep) =>
+        val typedCond = check(cond, typed.Type.Bool())
+        val typedThen = infer(thenp)
+        val typedElse = check(elsep, typedThen.info)
+        typed.Expr.If(typedCond, typedThen, typedElse)(using typedThen.info)
+      case Expr.Match(scrutinee, cases) => ???
+      case Expr.Tuple(info)             => ???
 
-  
+  def check(decl: Decl)(using Environment): typed.Decl = decl match
+    case Decl.Val(binder, body) =>
+      val typedBody   = infer(body)
+      val typedBinder = check(binder, typedBody.info)
+      typed.Decl.Val(typedBinder, typedBody)(using typedBody.info)
 
-  def check(expr: syntax.Expr, expected: typed.Type)(using
-      env: Env
-  ): Typecheck[typed.Expr] = ???
+    case Decl.Alias(name, typeParams, tpe) => ???
 
-  def check(decl: syntax.Decl)(using env: Env): Typecheck[typed.Decl] = ???
+    case Decl.FunGroup(fns) =>
+      val signatures = fns.map { case Decl.Fun(name, params, ret, _) =>
+        val tpe = params.foldRight(ret: typed.Type) {
+          case (Param.Type(name), ty) => typed.Type.TypeFn(name, ty)(using ???)
+          case (Param.Value(_, paramTy), ty) =>
+            typed.Type.Fn(paramTy, ty)(using ???)
+        }
+        name -> tpe
+      }
+      // todo: extend the env with the signatures of the fns
+      // then check the body of each fn
+      ???
 
-  def check(pattern: syntax.Pattern)(using env: Env): Typecheck[typed.Pattern] =
-    ???
-}
+    case Decl.DataGroup(fns) => ???
 
-object TypeChecker
+  def check(expr: syntax.Expr, tpe: typed.Type)(using
+      Environment
+  ): typed.Expr = ???
+
+  def check(pat: syntax.Pattern, tpe: typed.Type)(using
+      Environment
+  ): typed.Pattern = ???
+
+object TypeChecker extends Phase[syntax.Module, typed.Module]:
+  type Environment = Map[Symbol, typed.Type]
+  override def apply(in: syntax.Module)(using Context): typed.Module = ???
